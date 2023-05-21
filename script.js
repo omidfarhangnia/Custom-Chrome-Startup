4edc730c879b2429b43bdaff7506ef62f06867d8
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-05-21 23:30:42 +0330

    adding all the animation and structures

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// const btnId = document.getElementById("btnId");
-// btnId.addEventListener("click", () => {
-//     console.log("you clicked")
-//   window.open("https://www.google.com/search?q=hello+there");
-// // window.location.replace('https://www.google.com/search?q=hello+there')
-
-// });
-
 function wavesAnimeRight(className, delayValue) {
   const position = gsap.utils.random(0, 90);
   const bgColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -61,33 +53,131 @@ wavesAnimeLeft(".leftColorWaves1", 0);
 wavesAnimeLeft(".leftColorWaves2", 0.15);
 wavesAnimeLeft(".leftColorWaves3", 0.3);
 
+// default rotation animation
+let roundClockwize, counterClockwise;
+function startDefaultRotation() {
+  roundClockwize = gsap.to(".iconContainer", {
+    rotate: 360,
+    repeat: -1,
+    duration: 250,
+    ease: "linear",
+  });
+
+  counterClockwise = gsap.to(".icons, .searchIcon", {
+    rotate: -360,
+    repeat: -1,
+    duration: 250,
+    ease: "linear",
+  });
+}
+startDefaultRotation();
 // search icon animation
+const searchInput = document.querySelector(".searchInput");
 const searchIcon = document.querySelector(".searchIcon");
-searchIcon.addEventListener("click", () => {
-  const searchAnime = gsap.timeline();
-  searchAnime
-    .to(".icons", {
-      scale: .6,
-      opacity: 0,
-      stagger: .05,
-      duration: .1,
-    })
-    .set(".handBgImage", {
-      delay: .2,
-      backgroundSize: "0% 0%",
-    })
+const exitIcon = document.querySelector(".exitIcon");
+const searchAnime = gsap.timeline({
+  paused: true,
+  onComplete: () => {
+    searchIcon.removeEventListener("click", restartSearchAnime);
+    searchIcon.addEventListener("click", searchInBrowser);
+  },
+  onReverseComplete: () => {
+    searchIcon.removeEventListener("click", searchInBrowser);
+    searchIcon.addEventListener("click", restartSearchAnime);
+    startDefaultRotation()
+  },
 });
 
-gsap.to(".iconContainer", {
-  rotate: 360,
-  repeat: -1,
-  duration: 250,
-  ease: "linear"
-})
+searchAnime
+  .fromTo(
+    ".icons",
+    { scale: 1, opacity: 1 },
+    {
+      scale: 0.6,
+      opacity: 0,
+      stagger: 0.05,
+      duration: 0.1,
+    }
+  )
+  .set(".iconContainer, .icons, .searchIcon", { rotate: 0 })
+  .set(".icons", {
+    display: "none",
+  })
+  .set(".handBgImage", {
+    delay: 0.2,
+    backgroundSize: "0% 0%",
+  })
+  .fromTo(
+    ".searchIcon",
+    { top: "44%", left: "42%" },
+    {
+      left: 0,
+      duration: 1,
+      fontSize: "25px",
+      top: "48%",
+      delay: 0.5,
+    }
+  )
+  .set(".exitIcon", {
+    display: "block",
+  })
+  .set(".searchIcon", {
+    top: 0,
+    left: 0,
+  })
+  .set(".searchInput, .searchIcon", {
+    position: "relative",
+  })
+  .fromTo(
+    ".searchInput",
+    { scaleX: 0 },
+    {
+      scaleX: 1,
+      duration: 1,
+      ease: "linear",
+      delay: 0.6,
+    }
+  )
+  .fromTo(
+    ".exitIcon",
+    { opacity: 0 },
+    {
+      opacity: 1,
+      duration: 0.5,
+      linear: "ease",
+    }
+  );
 
-gsap.to(".icons, .searchIcon", {
-  rotate: -360,
-  repeat: -1,
-  duration: 250,
-  ease: "linear"
-})
+function restartSearchAnime() {
+  roundClockwize.kill();
+  counterClockwise.kill();
+  searchAnime.restart();
+}
+
+function reverseSearchAnime() {
+  searchAnime.reverse();
+  searchInput.value = "";
+}
+
+searchIcon.addEventListener("click", restartSearchAnime);
+
+exitIcon.addEventListener("click", reverseSearchAnime);
+
+function searchInBrowser() {
+  const inputValue = searchInput.value;
+  const splitedValue = inputValue.split(" ");
+  const joinedValue = splitedValue.join("+");
+  window.open(`https://www.google.com/search?q=${joinedValue}`);
+  searchInput.value = "";
+}
+
+searchInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    searchInBrowser();
+  }
+});
+
+// the structure of project
+
+// window.open("https://www.google.com/search?q=hello+there");
+// window.location.replace('https://www.google.com/search?q=hello+there')
