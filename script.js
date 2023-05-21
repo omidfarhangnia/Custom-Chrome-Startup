e1a7f24df030ad1014fb26024933db05f85954d0
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-05-21 14:13:06 +0330

    making the first structure

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnId = document.getElementById("btnId");
+btnId.addEventListener("click", () => {
+    console.log("you clicked")
+  window.open("https://www.google.com/search?q=hello+there");
+// window.location.replace('https://www.google.com/search?q=hello+there')
+
+});
